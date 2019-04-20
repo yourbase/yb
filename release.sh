d2c23c7f49ae1fb0713274eaca56a30044908cce
Yves Junqueira <yves.junqueira@gmail.com>
2019-04-20 22:57:35 +0000

    Add release script

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+APP="app_gtQEt1zkGMj"
+PROJECT="artificer"
+VERSION=$1
+
+equinox release \
+        --version=$VERSION \
+        --platforms="darwin_amd64 linux_amd64" \
+        --signing-key=${HOME}/equinox.key  \
+        --app="$APP" \
+        --token="$(cat ${HOME}/equinox.token)" "github.com/microclusters/${PROJECT}"
