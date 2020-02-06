cd4cb05772a58a50d1eb4d728600c5e0a517660f
Ridai Govinda Pombo <ridai.govinda@gmail.com>
2020-02-06 11:41:25 -0300

    builds on Linux, removed unused func

--- runtime/sandbox_windows.go
+++ runtime/sandbox_windows.go
@@ -2,5 +2,5 @@ package runtime
 
 // Currently just a passthrough
 func ExecInSandbox(command string, workingDir string) error {
-	return runtime.ExecToStdout(command, workingDir)
+	return ExecToStdout(command, workingDir)
 }
