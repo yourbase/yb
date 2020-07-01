be2dd74e401ef325574b22c30d913ff6abdec5f1
Ridai Govinda Pombo <ridai.govinda@gmail.com>
2020-07-01 14:08:03 -0300

    Correctly implements "Target" interface
    Without this we would get a SIGSEV when trying to call MkdirAsNeeded
    inside the build packs
    
    [ch1592]

--- runtime/containers.go
+++ runtime/containers.go
@@ -22,7 +22,6 @@ const (
 )
 
 type ContainerTarget struct {
-	Target
 	Container   *narwhal.Container
 	Environment []string
 	workDir     string
@@ -93,6 +92,12 @@ func (t *ContainerTarget) PathExists(ctx context.Context, path string) bool {
 	}
 
 	return true
+}
+
+func (t *ContainerTarget) MkdirAsNeeded(ctx context.Context, path string) error {
+	mkdirCmd := "mkdir -p " + path
+
+	return narwhal.ExecShell(ctx, narwhal.DockerClient(), t.Container.Id, mkdirCmd, nil)
 }
 
 func (t *ContainerTarget) String() string {
