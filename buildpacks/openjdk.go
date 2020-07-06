1f142c686434ec6d787a695875db770e438a1dcc
Ridai Govinda Pombo <ridai.govinda@yourbase.io>
2020-07-06 13:24:26 -0300

    Fixes to how we download OpenJDK versions (#104)
    Download URL for AdoptJDK is kind of a chalenge, we'll need to think
    this through.
    This fixes:
    1. JAVA_HOME setting is now correctly done
    2. PATH for $JAVA_HOME/bin is correct too
    
    This adds more test data, with versioning being done for either by
    giving 8.252.09 or 8.252+09 (e.g.).

--- buildpacks/openjdk.go
+++ buildpacks/openjdk.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
-        "strconv"
 
 	"github.com/johnewart/archiver"
 
@@ -14,45 +14,52 @@ import (
 	. "github.com/yourbase/yb/types"
 )
 
-
 type JavaBuildTool struct {
 	BuildTool
-	version string
-	spec    BuildToolSpec
+	version      string
+	spec         BuildToolSpec
 	majorVersion int64
 	minorVersion int64
 	patchVersion int64
-	subVersion string
+	subVersion   string
 }
 
 func NewJavaBuildTool(toolSpec BuildToolSpec) JavaBuildTool {
 
-	vparts := strings.SplitN(toolSpec.Version, "+", 2)
-	subVersion := ""
 	version := toolSpec.Version
-	if len(vparts) > 1 { 
+
+	vparts := strings.SplitN(version, "+", 2)
+	subVersion := ""
+	if len(vparts) > 1 {
 		subVersion = vparts[1]
 		version = vparts[0]
 	}
 
 	parts := strings.Split(version, ".")
-  c := len(parts)
 
-	var majorVersion int64
-	var minorVersion int64
-	var patchVersion int64
-
-	if c >= 1 {
-		majorVersion, _ = strconv.ParseInt(parts[0], 0, 64)
-		if c >= 2 {
-			minorVersion, _ = strconv.ParseInt(parts[1], 0, 64)
-			if c >= 3 {
-				patchVersion, _ = strconv.ParseInt(parts[2], 0, 64)
-			}
-		}
+	majorVersion, err := convertVersionPiece(parts, 0)
+	if err != nil {
+		log.Debugf("Error when parsing majorVersion %d: %v", majorVersion, err)
+	}
+	minorVersion, err := convertVersionPiece(parts, 1)
+	if err != nil {
+		log.Debugf("Error when parsing minorVersion %d: %v", minorVersion, err)
+	}
+	patchVersion, err := convertVersionPiece(parts, 2)
+	if err != nil {
+		log.Debugf("Error when parsing patchVersion %d: %v", patchVersion, err)
 	}
 
-	// Maybe we just require people format it with the build number? 
+	// Sometimes a patchVersion can represent a subVersion
+	// e.g.: java:8.252.09 instead of java:8.252+09
+	if majorVersion != 11 && majorVersion != 14 && subVersion == "" && patchVersion > 0 && patchVersion < 100 {
+		subVersion = fmt.Sprintf("%02d", patchVersion)
+	}
+
+	log.Debugf("majorVersion: %d, minorVersion: %d, patchVersion: %d, subVersion: %s",
+		majorVersion, minorVersion, patchVersion, subVersion)
+
+	// Maybe we just require people format it with the build number?
 	// Alternatively we can have a table of defaults somewhere
 	if subVersion == "" {
 		switch majorVersion {
@@ -67,7 +74,7 @@ func NewJavaBuildTool(toolSpec BuildToolSpec) JavaBuildTool {
 		case 12:
 			subVersion = "10"
 		case 13:
-			subVersion = "8"
+			subVersion = "9"
 		case 14:
 			subVersion = "36"
 		default:
@@ -75,21 +82,16 @@ func NewJavaBuildTool(toolSpec BuildToolSpec) JavaBuildTool {
 		}
 	}
 
-
 	tool := JavaBuildTool{
-		version: toolSpec.Version,
+		version:      toolSpec.Version,
 		majorVersion: majorVersion,
 		minorVersion: minorVersion,
 		patchVersion: patchVersion,
-		subVersion: subVersion,
-		spec:    toolSpec,
+		subVersion:   subVersion,
+		spec:         toolSpec,
 	}
 
 	return tool
-}
-
-func (bt JavaBuildTool) Version() string {
-	return bt.version
 }
 
 func (bt JavaBuildTool) InstallDir() string {
@@ -110,9 +112,24 @@ func (bt JavaBuildTool) JavaDir() string {
 
 	if opsys == "darwin" {
 		basePath = filepath.Join(basePath, "Contents", "Home")
-	}
 
+	}
 	return basePath
+}
+
+func convertVersionPiece(parts []string, index int) (piece int64, err error) {
+	if len(parts) >= index+1 {
+		trimmed := strings.TrimLeft(parts[index], "0")
+		piece, err = strconv.ParseInt(trimmed, 0, 64)
+		if err != nil {
+			err = fmt.Errorf("failed to parse %s: %v", parts[index], err)
+		}
+	}
+	return
+}
+
+func (bt JavaBuildTool) Version() string {
+	return bt.version
 }
 
 func (bt JavaBuildTool) Setup() error {
@@ -131,19 +148,25 @@ func (bt JavaBuildTool) Setup() error {
 func (bt JavaBuildTool) DownloadUrl() string {
 
 	// This changes pretty dramatically depending on major version :~(
-    // Using HotSpot version, we should consider an OpenJ9 option 
+	// Using HotSpot version, we should consider an OpenJ9 option
 	urlPattern := ""
 	if bt.majorVersion < 9 {
+		// TODO add openjdk8 OpenJ9 support
 		urlPattern = "https://github.com/AdoptOpenJDK/openjdk{{.MajorVersion}}-binaries/releases/download/jdk{{.MajorVersion}}u{{.MinorVersion}}-b{{.SubVersion}}/OpenJDK{{.MajorVersion}}U-jdk_{{.Arch}}_{{.OS}}_hotspot_{{.MajorVersion}}u{{.MinorVersion}}b{{.SubVersion}}.{{.Extension}}"
 	} else {
 		if bt.majorVersion < 14 {
-			urlPattern = "https://github.com/AdoptOpenJDK/openjdk{{ .MajorVersion }}-binaries/releases/download/jdk-{{ .MajorVersion }}.{{ .MinorVersion }}.{{ .PatchVersion }}%2B{{ .SubVersion }}/OpenJDK{{ .MajorVersion }}U-jdk_x64_linux_hotspot_{{ .MajorVersion }}.{{ .MinorVersion }}.{{ .PatchVersion }}_{{ .SubVersion }}.{{ .Extension }}"
+			// OpenJDK 9 has a whole other scheme
+			if bt.majorVersion == 9 && bt.subVersion == "181" {
+				urlPattern = "https://github.com/AdoptOpenJDK/openjdk{{ .MajorVersion }}-binaries/releases/download/jdk-{{ .MajorVersion }}%2B{{ .SubVersion }}/OpenJDK{{ .MajorVersion }}U-jdk_{{.Arch}}_{{.OS}}_hotspot_{{ .MajorVersion }}_{{ .SubVersion }}.{{ .Extension }}"
+			} else {
+				urlPattern = "https://github.com/AdoptOpenJDK/openjdk{{ .MajorVersion }}-binaries/releases/download/jdk-{{ .MajorVersion }}.{{ .MinorVersion }}.{{ .PatchVersion }}%2B{{ .SubVersion }}/OpenJDK{{ .MajorVersion }}U-jdk_{{.Arch}}_{{.OS}}_hotspot_{{ .MajorVersion }}.{{ .MinorVersion }}.{{ .PatchVersion }}_{{ .SubVersion }}.{{ .Extension }}"
+			}
 		} else {
 			// 14: https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14%2B36/OpenJDK14U-jdk_aarch64_linux_hotspot_14_36.tar.gz
-			urlPattern = "https://github.com/AdoptOpenJDK/openjdk{{ .MajorVersion }}-binaries/releases/download/jdk-{{ .MajorVersion }}%2B{{ .SubVersion }}/OpenJDK{{ .MajorVersion }}U-jdk_x64_linux_hotspot_{{ .MajorVersion }}_{{ .SubVersion }}.{{ .Extension }}"
+			urlPattern = "https://github.com/AdoptOpenJDK/openjdk{{ .MajorVersion }}-binaries/releases/download/jdk-{{ .MajorVersion }}%2B{{ .SubVersion }}/OpenJDK{{ .MajorVersion }}U-jdk_{{.Arch}}_{{.OS}}_hotspot_{{ .MajorVersion }}_{{ .SubVersion }}.{{ .Extension }}"
 		}
 	}
-	
+
 	arch := "x64"
 	extension := "tar.gz"
 
@@ -162,7 +185,7 @@ func (bt JavaBuildTool) DownloadUrl() string {
 		MajorVersion int64
 		MinorVersion int64
 		PatchVersion int64
-		SubVersion   string // not always an int, sometimes a float 
+		SubVersion   string // not always an int, sometimes a float
 		Extension    string
 	}{
 		operatingSystem,
