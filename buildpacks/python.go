fc83434e416c0f8024d7a49248897fe8acf6cfa5
Ridai Govinda Pombo <ridai.govinda@gmail.com>
2020-07-08 15:58:20 -0300

    Anaconda uses a new versioning scheme
    Python build pack can hard code it, but Anaconda needs better handling

--- buildpacks/python.go
+++ buildpacks/python.go
@@ -11,7 +11,8 @@ import (
 
 const (
 	anacondaToolVersion = "4.8.3"
-	anacondaURLTemplate = "https://repo.continuum.io/miniconda/Miniconda{{.PyNum}}-{{.Version}}-{{.OS}}-{{.Arch}}.{{.Extension}}"
+	// The version above needs a newer template
+	anacondaURLTemplate = "https://repo.continuum.io/miniconda/Miniconda{{.PyNum}}-py37_{{.Version}}-{{.OS}}-{{.Arch}}.{{.Extension}}"
 )
 
 type PythonBuildTool struct {
