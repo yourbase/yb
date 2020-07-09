339bf6613b994cda7d0a502e84507084b5ed3811
Ridai Govinda Pombo <ridai.govinda@yourbase.io>
2020-07-09 14:57:11 -0300

    A possibility slipped (#106)
    * A possibility slipped
    
    Better fix the release test
    
    * address feedback
    
    Co-authored-by: Ridai Govinda Pombo <ridai.govinda@gmail.com>

--- cli/build.go
+++ cli/build.go
@@ -66,7 +66,8 @@ func (b *BuildCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{
 			log.Errorf("Unable to parse argument: %v", err)
 			return subcommands.ExitFailure
 		}
-
+	}
+	if pkgName != "" {
 		pkg, err = ws.PackageByName(pkgName)
 		if err != nil {
 			log.Errorf("Unable to find package name %s: %v", pkgName, err)
