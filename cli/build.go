c76e92ec7f4f53bedc908d386f078115d2c3db1a
Ridai Govinda Pombo <ridai.govinda@gmail.com>
2020-07-28 10:13:14 -0300

    Revert "A possibility slipped (#106)"
    This reverts commit 339bf6613b994cda7d0a502e84507084b5ed3811.

--- cli/build.go
+++ cli/build.go
@@ -66,8 +66,7 @@ func (b *BuildCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{
 			log.Errorf("Unable to parse argument: %v", err)
 			return subcommands.ExitFailure
 		}
-	}
-	if pkgName != "" {
+
 		pkg, err = ws.PackageByName(pkgName)
 		if err != nil {
 			log.Errorf("Unable to find package name %s: %v", pkgName, err)
