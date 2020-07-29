a77ba30444753ca79820c1f47c7e6f70c7499c89
Ridai Govinda Pombo <ridai.govinda@yourbase.io>
2020-07-29 17:32:10 -0300

    Effectively introduces new build deps
    A build dependency defined inside a build target that isn't defined
    globally should be installed for use inside the build. So something
    like:
    
    ```
    dependencies:
      build:
        - go:1.13.4
    
    build_targets:
      - name: default
        commands:
          - go build
    
      - name: deploy
        dependencies:
          build:
            - python:3.8
            - go:1.14.6
    ```
    
    Will override the Go version and add Python 3.8.x as a build tool available
    for the "deploy" build target.
    
    NOTE: A build target doesn't use runtime dependencies, so there's no
    "run:" section inside a build target's dependencies section.
    
    Short-term fix for:
    "As Robson I'd like to run different flutter versionson
    different builds [ch1868]"

--- types/build_manifest.go
+++ types/build_manifest.go
@@ -4,8 +4,6 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
-	"sort"
-	"strings"
 )
 
 const DependencyChecksumLength = 12
@@ -25,7 +23,7 @@ func (b BuildManifest) IsTargetSandboxed(target BuildTarget) bool {
 }
 
 // XXX: Support more than one level? Intuitively that seems like it will breed un-needed complexity
-func (b BuildManifest) ResolveBuildTargets(targetName string) ([]BuildTarget, error) {
+func (b *BuildManifest) ResolveBuildTargets(targetName string) ([]BuildTarget, error) {
 	targetList := make([]BuildTarget, 0)
 
 	target, err := b.BuildTarget(targetName)
@@ -44,41 +42,8 @@ func (b BuildManifest) ResolveBuildTargets(targetName string) ([]BuildTarget, er
 	}
 
 	targetList = append(targetList, target)
-	err = b.mergeBuildDependencies(targetList)
-	if err != nil {
-		return targetList, err
-	}
 
 	return targetList, nil
-}
-
-// mergeBuildDependencies will override and also merge build packs defined for each BuildTarget when
-// resolving which ones should be built in ResolveBuildTargets above. It does so by checking if the
-// Manifest already defined the same `tool:version` as the one described inside the build target
-// TODO use cue instead, to replace all this error prone behavior
-func (b BuildManifest) mergeBuildDependencies(targetList []BuildTarget) error {
-	manifestSortedBuildDepList := b.Dependencies.Build
-	sort.Strings(manifestSortedBuildDepList)
-	for _, buildTarget := range targetList {
-		for _, dep := range buildTarget.Dependencies.Build {
-			for i, globalDep := range manifestSortedBuildDepList {
-				// Extract tool prefix
-				parts := strings.Split(dep, ":")
-				if len(parts) != 2 {
-					return fmt.Errorf("merging build deps: malformed build pack definition: %s", dep)
-				}
-				toolPrefix := parts[0]
-				if strings.HasPrefix(globalDep, toolPrefix) {
-					manifestSortedBuildDepList[i] = dep // override by replacing e.g. `flutter:xxx` with `flutter:yyy`
-				}
-			}
-			if i := sort.SearchStrings(manifestSortedBuildDepList, dep); i == len(manifestSortedBuildDepList) { // dep wasn't found
-				// merge
-				manifestSortedBuildDepList = append(manifestSortedBuildDepList, dep)
-			}
-		}
-	}
-	return nil
 }
 
 func (b BuildManifest) CIBuild(buildName string) (CIBuild, error) {
