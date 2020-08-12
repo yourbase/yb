c2e99cd68b5633377f2bb493475c8b9bcf1077a6
Ridai Govinda Pombo <ridai.govinda@yourbase.io>
2020-08-12 16:27:03 -0300

    Adds variables.sh Also moves Python dep into the build target
    Updates [ch1940]

---
+++ variables.sh
@@ -0,0 +1,38 @@
+# This should be included in both scripts, not invoked directly
+
+if [ -z "${VERSION:-}" ]; then
+  echo "Extracting version from tag ref ${YB_GIT_BRANCH:-}" 1>&2
+  VERSION="$(echo "${YB_GIT_BRANCH:-}" | sed -e 's|refs/tags/||g')"
+fi
+
+if [ -z "$VERSION" ]; then
+  echo "No version provided, won't release" 1>&2
+  exit 1
+fi
+
+if echo "$VERSION" | grep -vqo '^v'; then
+    echo "Doesn't start with a \"v\" when it should, not releasing"
+    exit 1
+fi
+
+if echo "$VERSION" | grep -qo '\-[a-z]\+[0-9]*$'; then
+    # Release candidate release
+    CHANNEL="preview"
+else
+    # Stable releases should not have a suffix
+    CHANNEL="stable"
+fi
+
+# Commit info
+COMMIT="${YB_GIT_COMMIT:-}"
+if [ -z "${COMMIT}" ]; then
+    # If git is installed
+    if hash git; then
+        COMMIT="$(git rev-parse HEAD)"
+    fi
+fi
+
+BUILD_COMMIT_INFO=""
+if [ -n "${COMMIT}" ]; then
+    BUILD_COMMIT_INFO=" -X 'main.commitSHA=$COMMIT'"
+fi
