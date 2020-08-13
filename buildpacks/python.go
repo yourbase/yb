6133f6129d4a1f53cf979c4e9fab2d5db1edddb3
Ridai Govinda Pombo <ridai.govinda@yourbase.io>
2020-08-13 14:51:51 -0300

    Revert "Bumps conda tool version (#148)"
    This reverts commit 46b8d15a102171c6b5fe8c5da6581a1994830579.

--- buildpacks/python.go
+++ buildpacks/python.go
@@ -10,8 +10,7 @@ import (
 )
 
 const (
-	// It's not totally needed, but bumping this version string can reduce time spent on conda's auto-update step
-	anacondaToolVersion = "4.8.4"
+	anacondaToolVersion = "4.8.3"
 	// The version above needs a newer template
 	anacondaURLTemplate = "https://repo.continuum.io/miniconda/Miniconda{{.PyNum}}-py37_{{.Version}}-{{.OS}}-{{.Arch}}.{{.Extension}}"
 )
