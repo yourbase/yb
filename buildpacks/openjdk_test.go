a14eaa9b2828556c97adb009a7c9ab9a0caa0458
Ross Light <light@yourbase.io>
2020-09-29 08:38:02 -0700

    Add GitHub Actions-based release process (#191)
    I'm using GitHub Actions primarily for macOS support: I firmly believe
    we should be running our tests on macOS, not just cross-compiling at
    release. This change retains the YourBase CI for main builds, but
    removes the release targets.
    
    I restructured the release automation scripts to be more modular and
    have more well-defined outputs. I've removed the Equinox process, since
    I'd like to revisit our packaging more broadly in future PRs.
    
    [Fixes ch2032]

--- buildpacks/openjdk_test.go
+++ buildpacks/openjdk_test.go
@@ -2,11 +2,22 @@ package buildpacks
 
 import (
 	"context"
+	"runtime"
 	"testing"
 )
 
 func TestOpenJDKUrlGeneration(t *testing.T) {
 	ctx := context.Background()
+	// TODO(light): Make OS/Arch a property of the execution environment.
+	var os string
+	switch runtime.GOOS {
+	case "darwin":
+		os = "mac"
+	case "linux":
+		os = "linux"
+	default:
+		t.Skipf("Unsupported OS: %s", runtime.GOOS)
+	}
 	// NOTE This can go on forever, so we better come up with a generic and reliable way of keep tools versions up to date
 	for _, data := range []struct {
 		version string
@@ -14,71 +25,71 @@ func TestOpenJDKUrlGeneration(t *testing.T) {
 	}{
 		{
 			version: "9+181",
-			url:     "https://github.com/AdoptOpenJDK/openjdk9-binaries/releases/download/jdk-9%2B181/OpenJDK9U-jdk_x64_linux_hotspot_9_181.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk9-binaries/releases/download/jdk-9%2B181/OpenJDK9U-jdk_x64_" + os + "_hotspot_9_181.tar.gz",
 		},
 		{
 			version: "8.252+09",
-			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u252b09.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_" + os + "_hotspot_8u252b09.tar.gz",
 		},
 		{
 			version: "8.252.9",
-			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u252b09.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_" + os + "_hotspot_8u252b09.tar.gz",
 		},
 		{
 			version: "8.252.09",
-			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u252b09.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_" + os + "_hotspot_8u252b09.tar.gz",
 		},
 		{
 			version: "8.242+08",
-			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u242b08.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_" + os + "_hotspot_8u242b08.tar.gz",
 		},
 		{
 			version: "8.242.8",
-			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u242b08.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_" + os + "_hotspot_8u242b08.tar.gz",
 		},
 		{
 			version: "8.242.08",
-			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u242b08.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_" + os + "_hotspot_8u242b08.tar.gz",
 		},
 		{
 			version: "8.232+09",
-			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u232-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u232b09.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u232-b09/OpenJDK8U-jdk_x64_" + os + "_hotspot_8u232b09.tar.gz",
 		},
 		{
 			version: "8.232.09",
-			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u232-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u232b09.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u232-b09/OpenJDK8U-jdk_x64_" + os + "_hotspot_8u232b09.tar.gz",
 		},
 		{
 			version: "8.232.9",
-			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u232-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u232b09.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u232-b09/OpenJDK8U-jdk_x64_" + os + "_hotspot_8u232b09.tar.gz",
 		},
 		{
 			version: "11.0.6",
-			url:     "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.6%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.6_10.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.6%2B10/OpenJDK11U-jdk_x64_" + os + "_hotspot_11.0.6_10.tar.gz",
 		},
 		{
 			version: "11.0.6+10",
-			url:     "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.6%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.6_10.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.6%2B10/OpenJDK11U-jdk_x64_" + os + "_hotspot_11.0.6_10.tar.gz",
 		},
 		{
 			version: "12.0.2+10",
-			url:     "https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.2%2B10/OpenJDK12U-jdk_x64_linux_hotspot_12.0.2_10.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.2%2B10/OpenJDK12U-jdk_x64_" + os + "_hotspot_12.0.2_10.tar.gz",
 		},
 		{
 			version: "13.0.2+8",
-			url:     "https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13.0.2%2B8/OpenJDK13U-jdk_x64_linux_hotspot_13.0.2_8.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13.0.2%2B8/OpenJDK13U-jdk_x64_" + os + "_hotspot_13.0.2_8.tar.gz",
 		},
 		{
 			version: "13.0.1+9",
-			url:     "https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13.0.1%2B9/OpenJDK13U-jdk_x64_linux_hotspot_13.0.1_9.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13.0.1%2B9/OpenJDK13U-jdk_x64_" + os + "_hotspot_13.0.1_9.tar.gz",
 		},
 		{
 			version: "14",
-			url:     "https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14%2B36/OpenJDK14U-jdk_x64_linux_hotspot_14_36.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14%2B36/OpenJDK14U-jdk_x64_" + os + "_hotspot_14_36.tar.gz",
 		},
 		{
 			version: "14+36",
-			url:     "https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14%2B36/OpenJDK14U-jdk_x64_linux_hotspot_14_36.tar.gz",
+			url:     "https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14%2B36/OpenJDK14U-jdk_x64_" + os + "_hotspot_14_36.tar.gz",
 		},
 	} {
 		bt := newJavaBuildTool(ctx, buildToolSpec{
