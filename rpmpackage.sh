d398b962d3c449af047aa36b3b8d1ac911c16e1a
Ross Light <light@yourbase.io>
2020-09-29 08:26:50 -0700

    Add RPM packaging automation
    [Fixes ch2578]

---
+++ rpmpackage.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Copyright 2020 YourBase Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# rpmpackage.sh builds a RPM binary package from the Go binary.
+
+set -euo pipefail
+
+if [[ $# -ne 0 ]]; then
+  echo "usage: VERSION=vX.Y.Z rpmpackage.sh" 1>&2
+  exit 64
+fi
+
+# Identify directories and create temporary staging directory.
+outdir="$( pwd )"
+srcroot="$(dirname "${BASH_SOURCE[0]}")"
+
+# Compute version.
+VERSION="${VERSION:-DEVELOPMENT}"
+rpmversion="${VERSION/#v/}"
+rpmversion="${rpmversion//-/\~}"
+
+# Verify that OS/architecture is something we handle.
+if [[ "$( go env GOOS )" != linux ]]; then
+  echo "Not running on Linux; quitting." 1>&2
+  exit 1
+fi
+case "$( go env GOARCH )" in
+  amd64)
+    rpmarch="x86_64"
+    ;;
+  *)
+    echo "TODO: Unknown architecture. GOARCH=$( go env GOARCH )" 1>&2
+    exit 1
+    ;;
+esac
+
+# Create binary package.
+( cd "$srcroot" && rpmbuild \
+  --build-in-place \
+  --define="version $rpmversion" \
+  --define="_rpmdir $outdir" \
+  -bb "$srcroot/yb.spec" ) 1>&2
+echo "${rpmarch}/yb-${rpmversion}-1-${rpmarch}.rpm"
+
---
+++ yb.spec
@@ -0,0 +1,27 @@
+Name:       yb
+Version:    %{version}
+Release:    1
+Summary:    Build tool optimized for local + remote development
+License:    ASL 2.0
+# TODO(light): Technically, we require these things to build.
+# However, we're arranging for these things to be present in the build
+# environment through different means.
+#BuildRequires: gcc
+#BuildRequires: golang >= 1.15
+Requires: glibc
+
+%description
+YourBase is a build tool that makes working on projects much more
+delightful. Stop worrying about dependencies, keep your CI build process
+in-sync with your local development process and onboard new team members
+with ease.
+
+%install
+mkdir -p %{buildroot}/usr/bin/
+# TODO(light): This could be under %build.
+VERSION=v%{version} ./build.sh %{buildroot}/usr/bin/yb
+chmod 755 yb %{buildroot}/usr/bin/yb
+
+%files
+%license LICENSE
+/usr/bin/yb
