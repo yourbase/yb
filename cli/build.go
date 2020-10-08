yourbase/yb

commit 2476bacd9d34f0dd0bf1f650bf21be189f0537fc

Upgrade yb to use latest Narwhal (#196)
Had to pin Docker to a weird commit to get macOS building.

[Fixes ch1470]

cli/build.go
@@ -17,11 +17,12 @@ import (
 	"sync"
 	"time"
 
+	docker "github.com/fsouza/go-dockerclient"
 	"github.com/johnewart/archiver"
-	"github.com/johnewart/narwhal"
 	"github.com/johnewart/subcommands"
 	"github.com/matishsiao/goInfo"
 	"github.com/yourbase/commons/xcontext"
+	"github.com/yourbase/narwhal"
 	ybconfig "github.com/yourbase/yb/config"
 	"github.com/yourbase/yb/packages"
 	"github.com/yourbase/yb/plumbing"
@@ -49,11 +50,11 @@ type BuildCmd struct {
 }
 
 type BuildConfiguration struct {
-	Target           types.BuildTarget
+	Target           *types.BuildTarget
 	TargetDir        string
 	ExecPrefix       string
 	ForceNoContainer bool
-	TargetPackage    packages.Package
+	TargetPackage    *packages.Package
 	BuildData        BuildData
 	ReuseContainers  bool
 }
@@ -137,6 +138,11 @@ func (b *BuildCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{
 	buildData := NewBuildData()
 
 	if !b.NoContainer {
+		dockerClient, err := docker.NewVersionedClient("unix:///var/run/docker.sock", "1.39")
+		if err != nil {
+			log.Errorf(ctx, "%v", err)
+			return subcommands.ExitFailure
+		}
 		target := primaryTarget
 		// Setup dependencies
 		containers := target.Dependencies.Containers
@@ -146,7 +152,7 @@ func (b *BuildCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{
 			return subcommands.ExitFailure
 		}
 		contextId := fmt.Sprintf("%s-%s", targetPackage.Name, target.Name)
-		sc, err := narwhal.NewServiceContextWithId(contextId, workDir)
+		sc, err := narwhal.NewServiceContextWithId(ctx, dockerClient, contextId, workDir)
 		if err != nil {
 			log.Errorf(ctx, "Couldn't create service context for dependencies: %v", err)
 			return subcommands.ExitFailure
@@ -159,18 +165,19 @@ func (b *BuildCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{
 			if !b.ReuseContainers {
 				log.Infof(ctx, "Not reusing containers, will teardown existing ones and clean up after ourselves")
 				defer Cleanup(xcontext.IgnoreDeadline(ctx), buildData)
-				if err := sc.TearDown(); err != nil {
+				if err := sc.TearDown(xcontext.IgnoreDeadline(ctx)); err != nil {
 					log.Errorf(ctx, "Couldn't terminate existing containers: %v", err)
 					// FAIL?
 				}
 			}
 
-			for _, c := range containers {
-				_, err := sc.StartContainer(c)
+			for _, cd := range containers {
+				c, err := sc.StartContainer(ctx, os.Stderr, cd.ToNarwhal())
 				if err != nil {
 					log.Errorf(ctx, "Couldn't start dependencies: %v", err)
 					return subcommands.ExitFailure
 				}
+				buildData.Containers.Containers[cd.Label] = c
 			}
 
 		}
@@ -211,7 +218,7 @@ func (b *BuildCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{
 		containerDef.Mounts = append(containerDef.Mounts, mount)
 
 		// Do our build in a container - don't do the build locally
-		err := b.BuildInsideContainer(ctx, target, containerDef, buildData, b.ExecPrefix, b.ReuseContainers)
+		err := b.BuildInsideContainer(ctx, target, containerDef.ToNarwhal(), buildData, b.ExecPrefix, b.ReuseContainers)
 		if err != nil {
 			log.Errorf(ctx, "Unable to build %s inside container: %v", target.Name, err)
 			return subcommands.ExitFailure
@@ -232,8 +239,8 @@ func (b *BuildCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{
 		log.Infof(ctx, "")
 		log.Infof(ctx, "Available side containers:")
 		for label, c := range primaryTarget.Dependencies.Containers {
-			ipv4 := buildData.Containers.IP(label)
-			log.Infof(ctx, "  * %s (using %s) has IP address %s", label, c.ImageNameWithTag(), ipv4)
+			ipv4 := buildData.Containers.IP(ctx, label)
+			log.Infof(ctx, "  * %s (using %s) has IP address %s", label, c.ToNarwhal().ImageNameWithTag(), ipv4)
 		}
 	}
 
@@ -359,44 +366,43 @@ func (b *BuildCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{
 func Cleanup(ctx context.Context, b BuildData) {
 	if b.Containers.ServiceContext != nil {
 		log.Infof(ctx, "Cleaning up containers...")
-		if err := b.Containers.ServiceContext.TearDown(); err != nil {
+		if err := b.Containers.ServiceContext.TearDown(ctx); err != nil {
 			log.Warnf(ctx, "Problem tearing down containers: %v", err)
 		}
 	}
 }
 
-func (b *BuildCmd) BuildInsideContainer(ctx context.Context, target types.BuildTarget, containerDef narwhal.ContainerDefinition, buildData BuildData, execPrefix string, reuseOldContainer bool) error {
-	// Perform build inside a container
+func (b *BuildCmd) BuildInsideContainer(ctx context.Context, target *types.BuildTarget, containerDef *narwhal.ContainerDefinition, buildData BuildData, execPrefix string, reuseOldContainer bool) error {
+	dockerClient := buildData.Containers.ServiceContext.DockerClient
 	image := target.Container.Image
 	log.Infof(ctx, "Using container image: %s", image)
-	buildContainer, err := buildData.Containers.ServiceContext.StartContainer(containerDef)
-
+	buildContainer, err := buildData.Containers.ServiceContext.FindContainer(ctx, containerDef)
 	if err != nil {
-		return fmt.Errorf("Failed trying to find container: %v", err)
+		return err
 	}
 
 	if buildContainer != nil {
 		log.Infof(ctx, "Found existing container %s", buildContainer.Id[0:12])
 
-		_ = buildContainer.Stop(0)
-
 		if !reuseOldContainer {
 			log.Infof(ctx, "Elected to not re-use containers, will remove the container")
 			// Try stopping it first if needed
-			if err = narwhal.RemoveContainerById(buildContainer.Id); err != nil {
+			_ = dockerClient.StopContainer(buildContainer.Id, 0)
+			err := dockerClient.RemoveContainer(docker.RemoveContainerOptions{
+				Context: ctx,
+				ID:      buildContainer.Id,
+			})
+			if err != nil {
 				return fmt.Errorf("Unable to remove existing container: %v", err)
 			}
+			buildContainer = nil
 		}
 	}
-
-	if buildContainer != nil && reuseOldContainer {
-		log.Infof(ctx, "Reusing existing build container %s", buildContainer.Id[0:12])
-	} else {
+	if buildContainer == nil {
 		log.Infof(ctx, "Creating a new build container...")
-		if c, err := buildData.Containers.ServiceContext.StartContainer(containerDef); err != nil {
+		buildContainer, err = buildData.Containers.ServiceContext.StartContainer(ctx, os.Stderr, containerDef)
+		if err != nil {
 			return fmt.Errorf("Couldn't create a new build container: %v", err)
-		} else {
-			buildContainer = c
 		}
 	}
 
@@ -404,17 +410,17 @@ func (b *BuildCmd) BuildInsideContainer(ctx context.Context, target types.BuildT
 	signal.Notify(c, os.Interrupt)
 	go func() {
 		<-c
-		buildContainer.Stop(0)
+		dockerClient.StopContainerWithContext(buildContainer.Id, 0, ctx)
 		os.Exit(0)
 	}()
 
-	defer buildContainer.Stop(0)
+	defer dockerClient.StopContainerWithContext(buildContainer.Id, 0, xcontext.IgnoreDeadline(ctx))
 
 	if err != nil {
 		return fmt.Errorf("Error creating build container: %v", err)
 	}
 
-	if err := buildContainer.Start(); err != nil {
+	if err := narwhal.StartContainer(ctx, dockerClient, buildContainer.Id); err != nil {
 		return fmt.Errorf("Unable to start container %s: %v", buildContainer.Id, err)
 	}
 
@@ -459,7 +465,8 @@ func (b *BuildCmd) BuildInsideContainer(ctx context.Context, target types.BuildT
 
 	// Upload and update CLI
 	log.Infof(ctx, "Uploading YB from %s to /yb", localYbPath)
-	if err := buildContainer.UploadFile(localYbPath, "yb", "/"); err != nil {
+	err = narwhal.UploadFile(ctx, dockerClient, buildContainer.Id, "/yb", localYbPath)
+	if err != nil {
 		return fmt.Errorf("Unable to upload YB to container: %v", err)
 	}
 
@@ -474,31 +481,62 @@ func (b *BuildCmd) BuildInsideContainer(ctx context.Context, target types.BuildT
 		}
 
 		log.Infof(ctx, "Updating YB in container from channel %s", ybChannel)
-		updateCmd := fmt.Sprintf("/yb update -channel=%s", ybChannel)
-		if err := buildContainer.ExecToStdoutWithEnv(updateCmd, containerWorkDir, buildData.EnvironmentVariables()); err != nil {
-			return fmt.Errorf("Aborting build, unable to run %s: %v", updateCmd, err)
+		err := execToStdoutWithEnv(
+			ctx,
+			dockerClient,
+			buildContainer, []string{"/yb", "update", "-channel=" + ybChannel},
+			containerWorkDir,
+			buildData.EnvironmentVariables(ctx),
+		)
+		if err != nil {
+			return err
 		}
 	}
 
-	cmdString := "/yb build"
-
+	argv := []string{"/yb", "build"}
 	if len(execPrefix) > 0 {
-		cmdString = fmt.Sprintf("%s -exec-prefix %s", cmdString, execPrefix)
+		argv = append(argv, "-exec-prefix", execPrefix)
 	}
+	argv = append(argv, "-no-container", target.Name)
 
-	cmdString = fmt.Sprintf("%s -no-container %s", cmdString, target.Name)
-
-	log.Infof(ctx, "Running %s in the container in directory %s", cmdString, containerWorkDir)
-
-	if err := buildContainer.ExecToStdoutWithEnv(cmdString, containerWorkDir, buildData.EnvironmentVariables()); err != nil {
-		log.Errorf(ctx, "Failed to run %s: %v", cmdString, err)
-		return fmt.Errorf("Aborting build, unable to run %s: %v", cmdString, err)
+	if err := execToStdoutWithEnv(ctx, dockerClient, buildContainer, argv, containerWorkDir, buildData.EnvironmentVariables(ctx)); err != nil {
+		return err
 	}
 
 	// Make sure our goroutine gets this from stdout
 	// TODO: There must be a better way...
 	time.Sleep(10 * time.Millisecond)
 
+	return nil
+}
+
+func execToStdoutWithEnv(ctx context.Context, client *docker.Client, c *narwhal.Container, argv []string, workDir string, env []string) error {
+	cmdString := strings.Join(argv, " ")
+	log.Infof(ctx, "Running in container directory %s: %s", workDir, cmdString)
+	opts := docker.CreateExecOptions{
+		Context:      ctx,
+		Container:    c.Id,
+		Cmd:          argv,
+		WorkingDir:   workDir,
+		Env:          env,
+		AttachStdout: true,
+		AttachStderr: true,
+	}
+	if c.Definition.ExecUserId != "" || c.Definition.ExecGroupId != "" {
+		opts.User = c.Definition.ExecUserId + ":" + c.Definition.ExecGroupId
+	}
+	e, err := client.CreateExec(opts)
+	if err != nil {
+		return fmt.Errorf("exec %s: %w", cmdString, err)
+	}
+	err = client.StartExec(e.ID, docker.StartExecOptions{
+		Context:      ctx,
+		OutputStream: os.Stdout,
+		ErrorStream:  os.Stdout,
+	})
+	if err != nil {
+		return fmt.Errorf("exec %s: %w", cmdString, err)
+	}
 	return nil
 }
 
@@ -591,15 +629,16 @@ func UploadBuildLogsToAPI(ctx context.Context, buf *bytes.Buffer) {
 // Build metadata; used for things like interpolation in environment variables
 type ContainerData struct {
 	ServiceContext *narwhal.ServiceContext
+	Containers     map[string]*narwhal.Container
 }
 
-func (c ContainerData) IP(label string) string {
-	// Check service context
-	if c.ServiceContext != nil {
-		if buildContainer, ok := c.ServiceContext.Containers[label]; ok {
-			if ipv4, err := buildContainer.IPv4Address(); err == nil {
-				return ipv4
-			}
+func (c ContainerData) IP(ctx context.Context, label string) string {
+	if buildContainer := c.Containers[label]; buildContainer != nil && c.ServiceContext != nil {
+		ip, err := narwhal.IPv4Address(ctx, c.ServiceContext.DockerClient, buildContainer.Id)
+		if err != nil {
+			log.Warnf(ctx, "Obtaining address for %s container: %v", label, buildContainer)
+		} else {
+			return ip.String()
 		}
 	}
 
@@ -612,14 +651,17 @@ func (c ContainerData) IP(label string) string {
 	return ""
 }
 
-func (c ContainerData) Environment() map[string]string {
+func (c ContainerData) Environment(ctx context.Context) map[string]string {
 	result := make(map[string]string)
 	if c.ServiceContext != nil {
-		for label, container := range c.ServiceContext.Containers {
-			if ipv4, err := container.IPv4Address(); err == nil {
-				key := fmt.Sprintf("YB_CONTAINER_%s_IP", strings.ToUpper(label))
-				result[key] = ipv4
+		for label, container := range c.Containers {
+			ipv4, err := narwhal.IPv4Address(ctx, c.ServiceContext.DockerClient, container.Id)
+			if err != nil {
+				log.Warnf(ctx, "Obtaining address for %s container: %v", label, err)
+				continue
 			}
+			key := fmt.Sprintf("YB_CONTAINER_%s_IP", strings.ToUpper(label))
+			result[key] = ipv4.String()
 		}
 	}
 	return result
@@ -641,7 +683,9 @@ func NewBuildData() BuildData {
 		}
 	}
 	return BuildData{
-		Containers:  ContainerData{},
+		Containers: ContainerData{
+			Containers: make(map[string]*narwhal.Container),
+		},
 		Environment: make(map[string]string),
 		originalEnv: originalEnv,
 	}
@@ -656,9 +700,9 @@ func (b BuildData) SetEnv(key string, value string) {
 	}
 }
 
-func (b BuildData) mergedEnvironment() map[string]string {
+func (b BuildData) mergedEnvironment(ctx context.Context) map[string]string {
 	result := make(map[string]string)
-	for k, v := range b.Containers.Environment() {
+	for k, v := range b.Containers.Environment(ctx) {
 		result[k] = v
 	}
 	for k, v := range b.Environment {
@@ -669,7 +713,7 @@ func (b BuildData) mergedEnvironment() map[string]string {
 
 func (b BuildData) ExportEnvironmentPublicly(ctx context.Context) {
 	log.Infof(ctx, "Exporting environment")
-	for k, v := range b.mergedEnvironment() {
+	for k, v := range b.mergedEnvironment(ctx) {
 		log.Infof(ctx, " * %s = %s", k, v)
 		os.Setenv(k, v)
 	}
@@ -677,7 +721,7 @@ func (b BuildData) ExportEnvironmentPublicly(ctx context.Context) {
 
 func (b BuildData) UnexportEnvironmentPublicly(ctx context.Context) {
 	log.Infof(ctx, "Unexporting environment")
-	for k := range b.mergedEnvironment() {
+	for k := range b.mergedEnvironment(ctx) {
 		if _, exists := b.originalEnv[k]; exists {
 			os.Setenv(k, b.originalEnv[k])
 		} else {
@@ -687,9 +731,9 @@ func (b BuildData) UnexportEnvironmentPublicly(ctx context.Context) {
 	}
 }
 
-func (b BuildData) EnvironmentVariables() []string {
+func (b BuildData) EnvironmentVariables(ctx context.Context) []string {
 	result := make([]string, 0)
-	for k, v := range b.mergedEnvironment() {
+	for k, v := range b.mergedEnvironment(ctx) {
 		result = append(result, fmt.Sprintf("%s=%s", k, v))
 	}
 	return result

types/types.go
@@ -1,7 +1,7 @@
 package types
 
 import (
-	"github.com/johnewart/narwhal"
+	"github.com/yourbase/narwhal"
 )
 
 const (
@@ -11,17 +11,17 @@ const (
 )
 
 type BuildManifest struct {
-	Dependencies DependencySet `yaml:"dependencies"`
-	Sandbox      bool          `yaml:"sandbox"`
-	BuildTargets []BuildTarget `yaml:"build_targets"`
-	Build        BuildTarget   `yaml:"build"`
-	Exec         ExecPhase     `yaml:"exec"`
-	Package      PackagePhase  `yaml:"package"`
-	CI           CIInfo        `yaml:"ci"`
+	Dependencies DependencySet  `yaml:"dependencies"`
+	Sandbox      bool           `yaml:"sandbox"`
+	BuildTargets []*BuildTarget `yaml:"build_targets"`
+	Build        *BuildTarget   `yaml:"build"`
+	Exec         *ExecPhase     `yaml:"exec"`
+	Package      *PackagePhase  `yaml:"package"`
+	CI           *CIInfo        `yaml:"ci"`
 }
 
 type CIInfo struct {
-	CIBuilds []CIBuild `yaml:"builds"`
+	CIBuilds []*CIBuild `yaml:"builds"`
 }
 
 type CIBuild struct {
@@ -40,59 +40,107 @@ type DependencySet struct {
 }
 
 type ExecPhase struct {
-	Name         string                      `yaml:"name"`
-	Dependencies ExecDependencies            `yaml:"dependencies"`
-	Container    narwhal.ContainerDefinition `yaml:"container"`
-	Commands     []string                    `yaml:"commands"`
-	Ports        []string                    `yaml:"ports"`
-	Environment  map[string][]string         `yaml:"environment"`
-	LogFiles     []string                    `yaml:"logfiles"`
-	Sandbox      bool                        `yaml:"sandbox"`
-	HostOnly     bool                        `yaml:"host_only"`
-	BuildFirst   []string                    `yaml:"build_first"`
+	Name         string               `yaml:"name"`
+	Dependencies ExecDependencies     `yaml:"dependencies"`
+	Container    *ContainerDefinition `yaml:"container"`
+	Commands     []string             `yaml:"commands"`
+	Ports        []string             `yaml:"ports"`
+	Environment  map[string][]string  `yaml:"environment"`
+	LogFiles     []string             `yaml:"logfiles"`
+	Sandbox      bool                 `yaml:"sandbox"`
+	HostOnly     bool                 `yaml:"host_only"`
+	BuildFirst   []string             `yaml:"build_first"`
+}
+
+type ContainerDefinition struct {
+	Image         string   `yaml:"image"`
+	Mounts        []string `yaml:"mounts"`
+	Ports         []string `yaml:"ports"`
+	Environment   []string `yaml:"environment"`
+	Command       string   `yaml:"command"`
+	WorkDir       string   `yaml:"workdir"`
+	Privileged    bool
+	PortWaitCheck PortWaitCheck `yaml:"port_check"`
+	Label         string        `yaml:"label"`
+	ExecUserId    string
+	ExecGroupId   string
+	Namespace     string
+	LocalWorkDir  string
+}
+
+func (def *ContainerDefinition) ToNarwhal() *narwhal.ContainerDefinition {
+	return &narwhal.ContainerDefinition{
+		Image:         def.Image,
+		Mounts:        def.Mounts,
+		Ports:         def.Ports,
+		Environment:   def.Environment,
+		Command:       def.Command,
+		WorkDir:       def.WorkDir,
+		Privileged:    def.Privileged,
+		PortWaitCheck: *def.PortWaitCheck.ToNarwhal(),
+		Label:         def.Label,
+		ExecUserId:    def.ExecUserId,
+		ExecGroupId:   def.ExecGroupId,
+		Namespace:     def.Namespace,
+		LocalWorkDir:  def.LocalWorkDir,
+	}
+}
+
+type PortWaitCheck struct {
+	Port         int `yaml:"port"`
+	Timeout      int `yaml:"timeout"`
+	LocalPortMap int
+}
+
+func (check *PortWaitCheck) ToNarwhal() *narwhal.PortWaitCheck {
+	return &narwhal.PortWaitCheck{
+		Port:         check.Port,
+		Timeout:      check.Timeout,
+		LocalPortMap: check.LocalPortMap,
+	}
 }
 
 type BuildDependencies struct {
-	Build      []string                               `yaml:"build"`
-	Containers map[string]narwhal.ContainerDefinition `yaml:"containers"`
+	Build      []string                        `yaml:"build"`
+	Containers map[string]*ContainerDefinition `yaml:"containers"`
 }
 
-func (b BuildDependencies) ContainerList() []narwhal.ContainerDefinition {
-	containers := make([]narwhal.ContainerDefinition, 0)
+func (b BuildDependencies) ContainerList() []*narwhal.ContainerDefinition {
+	containers := make([]*narwhal.ContainerDefinition, 0, len(b.Containers))
 	for label, c := range b.Containers {
 		c.Label = label
-		containers = append(containers, c)
+		containers = append(containers, c.ToNarwhal())
 	}
 	return containers
 }
 
 type ExecDependencies struct {
-	Containers map[string]narwhal.ContainerDefinition `yaml:"containers"`
+	Containers map[string]*ContainerDefinition `yaml:"containers"`
 }
 
-func (b ExecDependencies) ContainerList() []narwhal.ContainerDefinition {
-	containers := make([]narwhal.ContainerDefinition, 0)
+func (b ExecDependencies) ContainerList() []*narwhal.ContainerDefinition {
+	containers := make([]*narwhal.ContainerDefinition, 0, len(b.Containers))
 	for label, c := range b.Containers {
 		c.Label = label
-		containers = append(containers, c)
+		containers = append(containers, c.ToNarwhal())
 	}
 	return containers
 }
 
 type BuildTarget struct {
-	Name         string                      `yaml:"name"`
-	Container    narwhal.ContainerDefinition `yaml:"container"`
-	Tools        []string                    `yaml:"tools"`
-	Commands     []string                    `yaml:"commands"`
-	Artifacts    []string                    `yaml:"artifacts"`
-	CachePaths   []string                    `yaml:"cache_paths"`
-	Sandbox      bool                        `yaml:"sandbox"`
-	HostOnly     bool                        `yaml:"host_only"`
-	Root         string                      `yaml:"root"`
-	Environment  []string                    `yaml:"environment"`
-	Tags         map[string]string           `yaml:"tags"`
-	BuildAfter   []string                    `yaml:"build_after"`
-	Dependencies BuildDependencies           `yaml:"dependencies"`
+	Name         string               `yaml:"name"`
+	Container    *ContainerDefinition `yaml:"container"`
+	Tools        []string             `yaml:"tools"`
+	Commands     []string             `yaml:"commands"`
+	Artifacts    []string             `yaml:"artifacts"`
+	CachePaths   []string             `yaml:"cache_paths"`
+	Sandbox      bool                 `yaml:"sandbox"`
+	HostOnly     bool                 `yaml:"host_only"`
+	Root         string               `yaml:"root"`
+	Environment  []string             `yaml:"environment"`
+	Tags         map[string]string    `yaml:"tags"`
+	BuildAfter   []string             `yaml:"build_after"`
+	Dependencies BuildDependencies    `yaml:"dependencies"`
 }
 
 // A Project is a YourBase project as returned by the API.

cli/exec.go
@@ -7,8 +7,9 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/johnewart/narwhal"
+	docker "github.com/fsouza/go-dockerclient"
 	"github.com/johnewart/subcommands"
+	"github.com/yourbase/narwhal"
 	"github.com/yourbase/yb/plumbing"
 	"zombiezen.com/go/log"
 )
@@ -56,6 +57,12 @@ func (b *ExecCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{}
 	buildData := NewBuildData()
 
 	if len(containers) > 0 {
+		dockerClient, err := docker.NewVersionedClient("unix:///var/run/docker.sock", "1.39")
+		if err != nil {
+			log.Errorf(ctx, "%v", err)
+			return subcommands.ExitFailure
+		}
+
 		localContainerWorkDir := filepath.Join(targetPackage.BuildRoot(), "containers")
 		if err := os.MkdirAll(localContainerWorkDir, 0777); err != nil {
 			log.Errorf(ctx, "Couldn't create directory: %v", err)
@@ -64,7 +71,7 @@ func (b *ExecCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{}
 
 		log.Infof(ctx, "Will use %s as the dependency work dir", localContainerWorkDir)
 		log.Infof(ctx, "Starting %d dependencies...", len(containers))
-		sc, err := narwhal.NewServiceContextWithId("exec", targetPackage.BuildRoot())
+		sc, err := narwhal.NewServiceContextWithId(ctx, dockerClient, "exec", targetPackage.BuildRoot())
 		if err != nil {
 			log.Errorf(ctx, "Couldn't create service context for dependencies: %v", err)
 			return subcommands.ExitFailure
@@ -73,7 +80,7 @@ func (b *ExecCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{}
 		for _, c := range containers {
 			// TODO: avoid setting these here
 			c.LocalWorkDir = localContainerWorkDir
-			if _, err = sc.StartContainer(c); err != nil {
+			if _, err = sc.StartContainer(ctx, os.Stderr, c); err != nil {
 				log.Infof(ctx, "Couldn't start dependencies: %v\n", err)
 				return subcommands.ExitFailure
 			}

cli/helpers.go
@@ -16,9 +16,7 @@ func tracer() trace.Tracer {
 	return global.Tracer("github.com/yourbase/yb/cli")
 }
 
-func GetTargetPackageNamed(file string) (packages.Package, error) {
-	var targetPackage packages.Package
-
+func GetTargetPackageNamed(file string) (*packages.Package, error) {
 	if file == "" {
 		file = types.MANIFEST_FILE
 	}
@@ -30,29 +28,26 @@ func GetTargetPackageNamed(file string) (packages.Package, error) {
 		_, pkgName := filepath.Split(currentPath)
 		pkg, err := packages.LoadPackage(pkgName, currentPath)
 		if err != nil {
-			return packages.Package{}, fmt.Errorf("Error loading package '%s': %v\n\nSee %s\n", pkgName, err, types.DOCS_URL)
+			return nil, fmt.Errorf("Error loading package '%s': %w\n\nSee %s\n", pkgName, err, types.DOCS_URL)
 		}
-		targetPackage = pkg
-	} else {
-
-		workspace, err := workspace.LoadWorkspace()
-
-		if err != nil {
-
-			return packages.Package{}, fmt.Errorf("Could not find valid configuration: %v\n\nTry running in the package root dir or writing the YML config file (%s) if it is missing. See %s", err, file, types.DOCS_URL)
-		}
-
-		pkg, err := workspace.TargetPackage()
-		if err != nil {
-			return packages.Package{}, fmt.Errorf("Can't load workspace's target package: %v\n\nPackages under this Workspace may be missing a %s file or it's syntax is an invalid YML data. See %s", err, file, types.DOCS_URL)
-		}
-
-		targetPackage = pkg
+		return pkg, nil
 	}
 
-	return targetPackage, nil
+	workspace, err := workspace.LoadWorkspace()
+
+	if err != nil {
+
+		return nil, fmt.Errorf("Could not find valid configuration: %w\n\nTry running in the package root dir or writing the YML config file (%s) if it is missing. See %s", err, file, types.DOCS_URL)
+	}
+
+	pkg, err := workspace.TargetPackage()
+	if err != nil {
+		return nil, fmt.Errorf("Can't load workspace's target package: %w\n\nPackages under this Workspace may be missing a %s file or it's syntax is an invalid YML data. See %s", err, file, types.DOCS_URL)
+	}
+
+	return pkg, nil
 }
 
-func GetTargetPackage() (packages.Package, error) {
+func GetTargetPackage() (*packages.Package, error) {
 	return GetTargetPackageNamed(types.MANIFEST_FILE)
 }

cli/remote_build.go
@@ -93,7 +93,7 @@ func (p *RemoteCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface
 
 	manifest := targetPackage.Manifest
 
-	var target types.BuildTarget
+	var target *types.BuildTarget
 
 	if len(manifest.BuildTargets) == 0 {
 		target = manifest.Build

commit 9b36d6f26e9cdc791f7f29098ad1ec4dd1e1ff84

Coalesce nil ContainerDefinition and set defaults
Fixes nil pointer panics during container build introduced by
2476bacd9d34f0dd0bf1f650bf21be189f0537fc.

Removed fields from parsed ContainerDefinition that yb doesn't use.

cli/build.go
@@ -202,7 +202,7 @@ func (b *BuildCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{
 		log.Infof(ctx, "Executing build steps in container")
 
 		target := primaryTarget
-		containerDef := target.Container
+		containerDef := target.Container.ToNarwhal()
 		containerDef.Command = "/usr/bin/tail -f /dev/null"
 
 		// Append build environment variables
@@ -220,7 +220,7 @@ func (b *BuildCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{
 		containerDef.Mounts = append(containerDef.Mounts, mount)
 
 		// Do our build in a container - don't do the build locally
-		err := b.BuildInsideContainer(ctx, target, containerDef.ToNarwhal(), buildData, b.ExecPrefix, b.ReuseContainers)
+		err := b.BuildInsideContainer(ctx, target, containerDef, buildData, b.ExecPrefix, b.ReuseContainers)
 		if err != nil {
 			log.Errorf(ctx, "Unable to build %s inside container: %v", target.Name, err)
 			return subcommands.ExitFailure
@@ -376,7 +376,7 @@ func Cleanup(ctx context.Context, b BuildData) {
 
 func (b *BuildCmd) BuildInsideContainer(ctx context.Context, target *types.BuildTarget, containerDef *narwhal.ContainerDefinition, buildData BuildData, execPrefix string, reuseOldContainer bool) error {
 	dockerClient := buildData.Containers.ServiceContext.DockerClient
-	image := target.Container.Image
+	image := containerDef.Image
 	log.Infof(ctx, "Using container image: %s", image)
 	buildContainer, err := buildData.Containers.ServiceContext.FindContainer(ctx, containerDef)
 	if err != nil {

types/types.go
@@ -53,36 +53,33 @@ type ExecPhase struct {
 }
 
 type ContainerDefinition struct {
-	Image         string   `yaml:"image"`
-	Mounts        []string `yaml:"mounts"`
-	Ports         []string `yaml:"ports"`
-	Environment   []string `yaml:"environment"`
-	Command       string   `yaml:"command"`
-	WorkDir       string   `yaml:"workdir"`
-	Privileged    bool
+	Image         string        `yaml:"image"`
+	Mounts        []string      `yaml:"mounts"`
+	Ports         []string      `yaml:"ports"`
+	Environment   []string      `yaml:"environment"`
+	Command       string        `yaml:"command"`
+	WorkDir       string        `yaml:"workdir"`
 	PortWaitCheck PortWaitCheck `yaml:"port_check"`
 	Label         string        `yaml:"label"`
-	ExecUserId    string
-	ExecGroupId   string
-	Namespace     string
-	LocalWorkDir  string
 }
 
 func (def *ContainerDefinition) ToNarwhal() *narwhal.ContainerDefinition {
+	image := "yourbase/yb_ubuntu:18.04"
+	if def == nil {
+		return &narwhal.ContainerDefinition{Image: image}
+	}
+	if def.Image != "" {
+		image = def.Image
+	}
 	return &narwhal.ContainerDefinition{
-		Image:         def.Image,
-		Mounts:        def.Mounts,
-		Ports:         def.Ports,
-		Environment:   def.Environment,
+		Image:         image,
+		Mounts:        append([]string(nil), def.Mounts...),
+		Ports:         append([]string(nil), def.Ports...),
+		Environment:   append([]string(nil), def.Environment...),
 		Command:       def.Command,
 		WorkDir:       def.WorkDir,
-		Privileged:    def.Privileged,
 		PortWaitCheck: *def.PortWaitCheck.ToNarwhal(),
 		Label:         def.Label,
-		ExecUserId:    def.ExecUserId,
-		ExecGroupId:   def.ExecGroupId,
-		Namespace:     def.Namespace,
-		LocalWorkDir:  def.LocalWorkDir,
 	}
 }
 